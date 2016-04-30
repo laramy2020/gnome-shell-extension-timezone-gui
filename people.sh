906335856a71c2078843de7c406619d7cc62753c
Laramy <laramy2020@gmail.com>
2016-04-30 15:26:02 -0400

    ask for file.
    user can now add file file they would like to use.

--- people.sh
+++ people.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-#create timezone varable
-zoneinfo=$(cd /usr/share/zoneinfo && tree -fi | sed 's/\.\///' | sed '$ d' |  sed '$ d' | grep '\/' | tr '\n' '\|')
-#backup people.json
-cp ~/people.json ~/people.json.`date +"%m-%d-%y-_-%T"` 2>&1
-#Array set to newline seperation to allow naming structures like Last, First M.
-IFS=$'|'
-
-#Prompt user for info and output to a temp file (looking to add this into one command).
+#lets add some functions
+#check for non 0 exit codes caused mostly by users selecting the cancel option
+function cancel {
+if [ $? -eq 1 ]
+then
+	exit 1
+fi
+#check for programs needed by the script. reporting missing, only issue, cant gui report missing zenity
+}
+function iswhich {
+which $1
+if [ $? -eq 1 ]
+then
+	zenity --error --text "Command $1 not found."
+	exit 1
+fi
+}
+#for the prompt of adding a user!
+function forms {
 zenity --forms \
 --title="Add Friend" \
 --text="Enter information about your friend." \
@@ -17,10 +28,32 @@ zenity --forms \
 --add-entry="Github" \
 --add-entry="City" \
 --combo-values="$zoneinfo" > .tmp.info
-if [ $? -eq 1 ]
-then
-	exit 1
-fi
+}
+
+#check for programs/folders
+iswhich tree
+iswhich sed
+iswhich grep
+iswhich tr
+iswhich date
+iswhich zenity
+iszoneinfo=$(ls /usr/share | grep zoneinfo)
+cancel
+
+#ask for the file the user wants to use.
+jfile=$(zenity --file-selection --title="Select a File" --file-filter='json File (json) | *.json' --save --filename=$HOME/people.json)
+cancel
+
+#create timezone varable
+zoneinfo=$(cd /usr/share/zoneinfo && tree -fi | sed 's/\.\///' | sed '$ d' |  sed '$ d' | grep '\/' | tr '\n' '\|')
+#backup people.json
+cp $jfile $jfile.`date +"%m-%d-%y-_-%T"` 2>&1
+#Array set to newline seperation to allow naming structures like Last, First M.
+IFS=$'|'
+
+#Prompt user for info and output to a temp file (looking to add this into one command).
+forms
+cancel
 
 #import info to array
 tzinfo=(`cat ".tmp.info" ` )
@@ -35,22 +68,10 @@ tzhold=${tzinfo[1]}
 while [ $tzhold == ${tzinfo[1]} ] 
 do
 	zenity --error --text "A timezone must be selected."
-        zenity --forms \
-	--title="Add Friend" \
-	--text="Enter information about your friend." \
-	--add-entry="Name" \
-	--add-combo="Timezone" \
-	--add-entry="Avatar" \
-	--add-entry="Gravatar" \
-	--add-entry="Github" \
-	--add-entry="City" \
-	--combo-values="$zoneinfo" > .tmp.info
-	if [ $? -eq 1 ]
-	then
-		exit 1
-	fi
-       tzinfo=(`cat ".tmp.info" ` )
-       rm .tmp.info
+        forms
+	cancel
+        tzinfo=(`cat ".tmp.info" ` )
+        rm .tmp.info
 done
 
 #setup other varables
@@ -61,26 +82,26 @@ gravatar=${tzinfo[3]}
 city=${tzinfo[5]}
 
 #check to see if people.json is empty/exists. if so add starting brackets (ex [{)
-fexists=$(ls -la ~/people.json 2>&1)
-if [ "$fexists" == "ls: cannot access '$HOME/people.json': No such file or directory" ]
+fexists=$(ls -la $jfile 2>&1)
+if [ "$fexists" == "ls: cannot access '$jfile': No such file or directory" ]
 then
-	touch ~/people.json
+	touch $jfile
 fi
 
-nullcheck=$(<~/people.json)
+nullcheck=$(<$jfile)
 if [ -z "$nullcheck" ] 
 then
-	echo "[" >> ~/people.json
+	echo "[" >> $jfile
 fi
 
 
 # remove closing brackets at end of json file (ex }]) should be last two lines, also check to see if it is a new file.
-linecount=$( cat ~/people.json | wc -l )
+linecount=$( cat $jfile | wc -l )
 if [ $linecount -gt 2 ]
 then
-	sed -i '$ d' ~/people.json
-	sed -i '$ d' ~/people.json
-        echo "        }," >> ~/people.json
+	sed -i '$ d' $jfile
+	sed -i '$ d' $jfile
+        echo "        }," >> $jfile
 fi
 	
 
@@ -121,10 +142,10 @@ fi
 sed -i '/^ *$/d' ~/.people.json.template
 
 #cat the files togather
-cat ~/people.json ~/.people.json.template > ~/.temp.json
+cat $jfile ~/.people.json.template > ~/.temp.json
 
 #create new people.json
-rm ~/people.json
-mv ~/.temp.json ~/people.json
+rm $jfile
+mv ~/.temp.json $jfile
 rm ~/.people.json.template
-echo "]" >> ~/people.json
+echo "]" >> $jfile
